dce73acf61c74263e9f6eba7480b44f774c66727
mqashoo77 <mohammadqashoo77@gmail.com>
2022-07-03 02:49:01 +0300

    Editing a Main Activity View

--- app/src/main/java/com/example/todo_listapp/MainActivity.java
+++ app/src/main/java/com/example/todo_listapp/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         builder.show();
+
     }*/
 
     public static void redirectActicity(Activity activity, Class aclass) {
